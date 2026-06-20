4bd9fb515fb4a1477550a7573be578879ef1e5c6
Juhi Rathor <iamjuhirathor@gmail.com>
2026-06-20 18:29:30 +0530

    Configure DNS to prefer IPv4 first to fix Render ENETUNREACH SMTP error

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 
 const app = require('./app');
 const { connectDB, disconnectDB } = require('./config/db.config');
